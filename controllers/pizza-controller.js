add44249149625fa51bd4f43e592b0710c4ac632
Jesus Manteca <jesusmanteca@gmail.com>
2020-12-02 08:05:45 -0600

    integrated front-end to back-end with mongoose

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,4 +1,5 @@
 const { Pizza } = require('../models');
+// const Pizza = require('../models/Pizza');
 
 const pizzaController = {
     // the functions will go in here as methods
@@ -6,6 +7,12 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -16,8 +23,12 @@ const pizzaController = {
     // get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbPizzaData => {
-                // If no pizza is found, send 404
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
                     return;
